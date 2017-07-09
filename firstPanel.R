00c02865f01e014c5fa68f3fe1e1516f74e388bf
atv22 <a@a.nl>
2017-07-09 18:38:17 +0200

    Start new match and store score
    Start new match and store score

--- firstPanel.R
+++ firstPanel.R
@@ -1,6 +1,10 @@
 firstPanel <- tabPanel("New match",
     fluidPage(
-      titlePanel("ADR Tafelvoetbalstatistiek p2")
+      column(2,
+      selectInput("select", label = h3("Select box"), 
+                         choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                         selected = 1),
+      actionButton("newMatch", label = "Start new match"))
     )
 )
 
--- server.R
+++ server.R
@@ -1,10 +1,27 @@
 server <- function(input, output,session) {
-source("googlesheet_setup.r")
-  
-  rv <- reactiveValues(score=paste("A 0","-","0 B"))  
-  
-# tab1: New Match ---------------------------------------------------------
 
+  
+rv <- reactiveValues(score=paste("A 0","-","0 B"),
+                       matches="")  
+source("googlesheet_setup.r")
+    
+# tab1: New Match ---------------------------------------------------------
+observeEvent(input$newMatch,{
+  rv$matches <- sheet %>% gs_read("matches")
+  df <- data.frame(match_id=max(matches$match_id)+1,
+                   date=Sys.time(),
+                   player_a1="testa1",
+                   position_a1="voor",
+                   player_a2="testa2",
+                   position_a2="achter",
+                   player_b1="testb1",
+                   position_b1="achter",
+                   player_b2="testa1",
+                   position_b2="voor")
+  gs_add_row(sheet,ws="matches",input=df[1,],verbose=F)
+  rv$matches <- sheet %>% gs_read("matches")
+
+  })
 
 
 # tab 2: Keep score -------------------------------------------------------
@@ -20,41 +37,45 @@ source("googlesheet_setup.r")
       select(player_a1,player_a2,player_b1,player_b2)%>%
       summarise_all(funs(sum))%>%
       mutate(team_a= player_a1+player_a2, team_b=player_b1+player_b2)
-    rv$score <- paste("A", scores_match$team_a ,"-",scores_match$team_b,"B")
     }
   
   observeEvent(input$score_a1,{
-    df <- data.frame(t = Sys.time(),match_id=0,score_a1=1,score_a2=0,score_b1=0,score_b2=0)
+    df <- data.frame(t = Sys.time(),match_id=max(rv$matches$match_id),score_a1=1,score_a2=0,score_b1=0,score_b2=0)
     gs_add_row(sheet,ws="scores",input=df[1,],verbose=F)
     scores <- sheet %>% gs_read("scores")
     RecalculateScore()
+    Sys.sleep(1)
+    rv$score <- paste("A", scores_match$team_a ,"-",scores_match$team_b,"B")
   })
   
   observeEvent(input$score_a2,{
-    df <- data.frame(t = Sys.time(),match_id=0,score_a1=0,score_a2=1,score_b1=0,score_b2=0)
+    df <- data.frame(t = Sys.time(),match_id=max(rv$matches$match_id),score_a1=0,score_a2=1,score_b1=0,score_b2=0)
     gs_add_row(sheet,ws="scores",input=df[1,],verbose=F)
     scores <- sheet %>% gs_read("scores")
     RecalculateScore()
+    Sys.sleep(1)
+    rv$score <- paste("A", scores_match$team_a ,"-",scores_match$team_b,"B")
   })
   
   observeEvent(input$score_b1,{
-    df <- data.frame(t = Sys.time(),match_id=0,score_a1=0,score_a2=0,score_b1=1,score_b2=0)
+    df <- data.frame(t = Sys.time(),match_id=max(rv$matches$match_id),score_a1=0,score_a2=0,score_b1=1,score_b2=0)
     gs_add_row(sheet,ws="scores",input=df[1,],verbose=F)
     scores <- sheet %>% gs_read("scores")
     RecalculateScore()
+    Sys.sleep(1)
+    rv$score <- paste("A", scores_match$team_a ,"-",scores_match$team_b,"B")
   })
     
   observeEvent(input$score_b2,{
-    df <- data.frame(t = Sys.time(),match_id=0,score_a1=0,score_a2=0,score_b1=0,score_b2=1)
+    df <- data.frame(t = Sys.time(),match_id=max(rv$matches$match_id),score_a1=0,score_a2=0,score_b1=0,score_b2=1)
     gs_add_row(sheet,ws="scores",input=df[1,],verbose=F)
     scores <- sheet %>% gs_read("scores")
     RecalculateScore()
+    rv$score <- paste("A", scores_match$team_a ,"-",scores_match$team_b,"B")
+    Sys.sleep(1)
   })
     
-observe({
- output$score <- renderText(rv$score) 
-})  
-
+output$score <- renderText(rv$score)
 
 # tab 3: Add player -------------------------------------------------------
 
